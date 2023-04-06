2fbb33dea07e7652e49432c2348746f4b91ad166
krassot1n <krassotin68@gmail.com>
2023-04-06 17:30:54 +0800

    Проект доделан

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        Car[] cars = {
+                new Car("car1", 4),
+                new Car("car2", 4)};
+
+        Truck[] trucks = {
+                new Truck("truck1", 6),
+                new Truck("truck2", 8)};
+
+        Bicycle[] bicycles = {
+                new Bicycle("bicycle1", 2),
+                new Bicycle("bicycle2", 2)};
+
+        ServiceStation station = new ServiceStation();
+        station.repair(cars);
+        station.repair(bicycles);
+        station.repair(trucks);
     }
 }
---
+++ src/NecessaryRepairs.java
@@ -0,0 +1,8 @@
+public interface NecessaryRepairs {
+    public void repair(Car[] cars);
+
+    public void repair(Bicycle[] bicycles);
+
+    public void repair(Truck[] trucks);
+
+}
